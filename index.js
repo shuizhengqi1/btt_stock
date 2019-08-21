459791e25ac90e3faaaabf1261a008941fdf7ad4
xianyu <xianyu@gegejia.com>
2019-08-21 12:35:07 +0800

    init

---
+++ index.js
@@ -0,0 +1,25 @@
+const iconv = require("iconv-lite");
+const request = require('request')
+const express = require('express')
+const app = express()
+
+app.get('/stock', (req, response) => {
+
+    var url = "https://hq.sinajs.cn/list=s_" + req.query.stockCode
+    request({
+        url: url,
+        encoding: null
+    }, (err, res, body) => {
+        if (body != null) {
+            var result = iconv.decode(body, 'GBK')
+            var resultList = result.split(',')
+            response.send({
+                count: resultList[1],
+                percent: resultList[3]
+            })
+
+        }
+    })
+})
+
+app.listen(2000)
